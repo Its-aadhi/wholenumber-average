204e66812a121801784aa8d5a4f086a51743bade
Aadhi <95705120+Its-aadhi@users.noreply.github.com>
2025-01-13 20:52:14 -0500

    Create wholenumber.c
    Convert whole number into average

---
+++ wholenumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+int main (void)
+
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    float average = 0;
+
+    printf("Please enter the first number: ");
+    scanf("%d", &a);
+    printf("Please enter the second number: ");
+    scanf("%d", &b);
+    printf("Please enter the third number: ");
+    scanf("%d", &c);
+
+    average = (a + b + c) / 3.0;
+
+    printf("The average of the three numbers is %.2f\n", average);
+    return 0;
+
+
+}
